14a630c346459f75112d395c15ac161985fd987a
leejonghoon <jonghoon.1738@gmail.com>
2022-10-28 10:52:16 +0900

    초기 커밋.

---
+++ IndexNumberBinder/AutoCsvIndexBinder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexNumberBinder
+{
+    public class AutoCsvIndexBinder
+    {
+        private readonly int startIndex;
+
+        public AutoCsvIndexBinder(int startIndex)
+        {
+            this.startIndex = startIndex;
+        }
+        public void Bind(Type model)
+        {
+            int i = this.startIndex;
+            model.GetType().GetProperties().ToList().ForEach(x =>
+            {
+                Console.WriteLine($"md.{x.Name} = {getTypeString(x.PropertyType, i)}");
+                i++;
+            });
+
+        }
+
+        public string getTypeString(Type type, int idx)
+        {
+            //valueData[{ i}]
+            string result;
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Int32:
+                    result = $"Convert.ToInt32(valueData[{idx}]);";
+                    break;
+                case TypeCode.DateTime:
+                    result = $"Convert.ToDateTime(valueData[{idx}]);";
+                    break;
+                case TypeCode.Double:
+                    result = $"Convert.ToDouble(valueData[{idx}]);";
+                    break;
+                default:
+                    result = $"valueData[{idx}];";
+                    break;
+            }
+
+            return result;
+        }
+    }
+
+
+}
---
+++ IndexNumberBinder/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IndexNumberBinder;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+AutoCsvIndexBinder binder = new AutoCsvIndexBinder(0);
+TestClass tstClass = new TestClass();
+binder.Bind(tstClass.GetType());
+Console.ReadLine();
